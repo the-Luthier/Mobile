f23e79015aa32fccaa59f8bdb3b63e3a937de7fc
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-04-26 11:42:29 +0300

    12345

--- NetGmobile/mobileapp/decorators.py
+++ NetGmobile/mobileapp/decorators.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.decorators import user_passes_test
 from functools import wraps
 from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Profile 
+
+
+from django.shortcuts import redirect
+from functools import wraps
 from .models import Profile
 
-def verification_required(f):
-    return user_passes_test(lambda u: u.is_verified, login_url='/verify')(f) 
+def verification_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        try:
+            profile = request.user.profile
+            if profile.is_verified:
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect('/verify')
+        except Profile.DoesNotExist:
+            return redirect('/verify')
+
+    return _wrapped_view
+
+
--- NetGmobile/mobileapp/models.py
+++ NetGmobile/mobileapp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=200, blank=False)
