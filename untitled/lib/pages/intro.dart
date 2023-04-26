977b2a21210b072b07379be67ada9c8b8a4f1e78
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-04-26 10:43:48 +0300

    1

---
+++ untitled/lib/pages/intro.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    );
+
+    _animationController.forward().then((_) {
+      Timer(const Duration(milliseconds: 2000), () {
+        Navigator.of(context).pushReplacementNamed('/home');
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FadeTransition(
+              opacity: _animation,
+              child: Image.asset(
+                'assets/images/your_logo.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
