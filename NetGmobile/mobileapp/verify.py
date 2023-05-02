5e03dcaf51faf74c593e95526c3ac5e10bef4470
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-02 12:46:12 +0300

    çok da önemli olmayabilir

--- NetGmobile/mobileapp/verify.py
+++ NetGmobile/mobileapp/verify.py
@@ -3,6 +3,8 @@ from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 from dotenv import load_dotenv
 
+load_dotenv()
+
 client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
 verify = client.verify.services(os.environ["TWILIO_VERIFY_SERVICE_SID"])
 
