76424d6dd9d7f337334c5029313ea8d471f52400
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-04 17:50:50 +0300

    send notifications completed

--- netg/mobileapp/admin.py
+++ netg/mobileapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Profile
+from .models import Device
+from .utils import send_push_notifications
 
-# Register your models here.
+def send_notifications(modeladmin, request, queryset):
+    profile_ids = queryset.values_list('id', flat=True)
+    device_tokens = Device.objects.filter(profile__in=profile_ids).values_list('device_token', flat=True)
+    
+    title = "Your notification title"
+    message = "Your notification message"
+    
+    send_push_notifications(device_tokens, title, message)
+
+send_notifications.short_description = "Send notifications to selected profiles"
+
+class CustomProfileAdmin(admin.ModelAdmin):
+    actions = [send_notifications]
+
+admin.site.unregister(Profile)
+admin.site.register(Profile, CustomProfileAdmin)
