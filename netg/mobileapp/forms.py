196b6f592d79442f57685396ce5d5df509757ee0
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-05 10:54:40 +0300

    1234

--- netg/mobileapp/forms.py
+++ netg/mobileapp/forms.py
@@ -15,7 +15,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm as BasePasswordResetForm
 from mobileapp.models import Profile
 from django.contrib.auth import authenticate, get_user_model
-from django.contrib.auth.models import AbstractUser
+
 
 
 
